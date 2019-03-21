95fbfc9943634a7a6904c0011d81cd034b4f2860
Ulises Quintanilla <ulisesqg@gmail.com>
2019-03-20 17:19:03 -0700

    Users app.

--- ulisesqg/settings/base.py
+++ ulisesqg/settings/base.py
@@ -33,6 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -106,3 +108,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
